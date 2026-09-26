015fb13ae14ba3158d2bef6646feb431d80a6d58
nohchan <nohchan@gantan.net>
2026-09-26 11:35:09 +0900

    Update 3dssettings.cpp

--- source/3dssettings.cpp
+++ source/3dssettings.cpp
@@ -238,7 +238,7 @@ void settings3dsUpdate(bool includeGameSettings)
 
 const char *settings3dsGetAppVersion(const char *prefix, const char *suffix) {
     static char version[64];
-
+ 
     if (VERSION_MICRO > 0) {
         snprintf(version, sizeof(version), "%s%d.%d.%d+%s", prefix, VERSION_MAJOR, VERSION_MINOR, VERSION_MICRO, suffix != NULL ? suffix : "");
     } else {
